ce0cb2649180e8291807fbd62a9225d18cb5dbc0
pizhanska <pizhanska29@gmail.com>
2018-10-11 16:10:37 +0300

    update Base Page

--- src/main/java/com/aws/framework/core/pages/BasePage.java
+++ src/main/java/com/aws/framework/core/pages/BasePage.java
@@ -2,9 +2,12 @@ package com.aws.framework.core.pages;
 
 import com.aws.framework.core.utils.DriverFactory;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebElement;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
@@ -16,65 +19,82 @@ public class BasePage {
 
     protected WebDriverWait wait;
     protected JavascriptExecutor js;
+    protected DriverFactory.DriverType type;
+
+
+    public BasePage(DriverFactory.DriverType type){
+        this.type = type;
+        switch (type){
+            case CHROME:
+                PageFactory.initElements(DriverFactory.getInstance().getDriver(), this);
+                break;
+            case ANDROID:
+                PageFactory.initElements(new AppiumFieldDecorator(DriverFactory.getInstance().getDriver()), this);
+                break; }
+
+    }
+
+    public BasePage(){}
+
 
     //Waits
-    public void waitElementToBeClicked(DriverFactory.DriverType type, RemoteWebElement webElement, int sec) {
-        wait = new WebDriverWait(DriverFactory.getDriver(type), sec);
+    public void waitElementToBeClicked(WebElement webElement, int sec) {
+        wait = new WebDriverWait(DriverFactory.getInstance().getDriver(), sec);
         wait.until(ExpectedConditions.elementToBeClickable(webElement));
     }
 
-    public void waitElementToBeSelected(DriverFactory.DriverType type,RemoteWebElement webElement, int sec) {
-        wait = new WebDriverWait(DriverFactory.getDriver(type), sec);
+    public void waitElementToBeSelected(WebElement webElement, int sec) {
+        wait = new WebDriverWait(DriverFactory.getInstance().getDriver(), sec);
         wait.until(ExpectedConditions.elementToBeSelected(webElement));
     }
 
-    public void waitElementToBeVisible(DriverFactory.DriverType type,RemoteWebElement webElement, int sec) {
-        wait = new WebDriverWait(DriverFactory.getDriver(type), sec);
+    public void waitElementToBeVisible(WebElement webElement, int sec) {
+        wait = new WebDriverWait(DriverFactory.getInstance().getDriver(), sec);
         wait.until(ExpectedConditions.visibilityOf(webElement));
     }
 
-    public void waitForAlert(DriverFactory.DriverType type, int sec) {
-        wait = new WebDriverWait(DriverFactory.getDriver(type), sec);
+    public void waitForAlert(int sec) {
+        wait = new WebDriverWait(DriverFactory.getInstance().getDriver(), sec);
         wait.until(ExpectedConditions.alertIsPresent());
     }
 
-    public void waitForPageLoad(DriverFactory.DriverType type, int sec) {
-        new WebDriverWait(DriverFactory.getDriver(type), sec).until((ExpectedCondition<Boolean>) wd ->
+    public void waitForPageLoad(int sec) {
+        new WebDriverWait(DriverFactory.getInstance().getDriver(), sec).until((ExpectedCondition<Boolean>) wd ->
                 ((JavascriptExecutor) wd).executeScript("return document.readyState").equals("complete"));
     }
 
     //Swipe and scroll
-    public void scrollPageUntilElementIsVisible(DriverFactory.DriverType type, RemoteWebElement element) {
+    public void scrollPageUntilElementIsVisible(WebElement element) {
         if (type == DriverFactory.DriverType.CHROME) {
-            js = (JavascriptExecutor) DriverFactory.getDriver(type);
+            js = (JavascriptExecutor) DriverFactory.getInstance().getDriver();
             js.executeScript("arguments[0].scrollIntoView();", element);
         } else {
-            String elementID = element.getId();
+            String elementID = ((RemoteWebElement)element).getId();
             HashMap<String, String> scrollObject = new HashMap<>();
             scrollObject.put("element", elementID);
             scrollObject.put("toVisible", "not an empty string");
-            WebDriver driver = DriverFactory.getDriver(type);
+            WebDriver driver = DriverFactory.getInstance().getDriver();
             AppiumDriver appiumDriver = (AppiumDriver) driver;
             appiumDriver.executeScript("mobile:scroll", scrollObject);
         }
     }
 
-    public void scrollDownToThePageBottom(DriverFactory.DriverType type, RemoteWebElement element) {
+    public void scrollDownToThePageBottom(WebElement element) {
         if (type == DriverFactory.DriverType.CHROME) {
-            js = (JavascriptExecutor) DriverFactory.getDriver(type);
+            js = (JavascriptExecutor) DriverFactory.getInstance().getDriver();
             js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
         } else {
-            js = (JavascriptExecutor) DriverFactory.getDriver(type);
+            js = (JavascriptExecutor) DriverFactory.getInstance().getDriver();
             HashMap scrollObjects = new HashMap();
             scrollObjects.put("direction", "down");
-            scrollObjects.put("element", element.getId());
+            scrollObjects.put("element", ((RemoteWebElement)element).getId());
             js.executeScript("mobile: scroll", scrollObjects);
         }
     }
 
     //Get title
-    public String getPageTitle(DriverFactory.DriverType type) {
-        String title = DriverFactory.getDriver(type).getTitle();
+    public String getPageTitle() {
+        String title = DriverFactory.getInstance().getDriver().getTitle();
         return title;
     }
 }
